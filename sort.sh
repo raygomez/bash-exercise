a57d7126300a36dc923ea81f46d5db11f2e84b4e
Ray Gomez <ray.gomez@nokia.com>
2015-10-09 16:10:12 +0800

    added sort

---
+++ sort.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+if [ $# -lt 1 ]
+then 
+  echo 'It need at least one argument.'
+  echo "Usage:./sort.sh [asc|desc] numbers..."
+  exit -1
+fi
+
+arg1=$1
+
+if [ "$arg1" == 'asc' ]
+then 
+  mode=
+
+elif [ "$arg1" == 'desc' ]
+then 
+  mode='-r'
+
+else
+  echo "Usage:./sort.sh [asc|desc] numbers..."
+  exit -1
+fi
+
+
+shift
+nums=$@
+
+echo $@ | tr ' ' '\n' | sort ${mode}
