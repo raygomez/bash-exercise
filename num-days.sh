12df3efe2c692fc54704c9546e4f3c543d7760f6
Ray Gomez <ray.gomez@nokia.com>
2015-10-12 18:54:16 +0800

    added case

--- num-days.sh
+++ num-days.sh
@@ -48,3 +48,19 @@ then
 else
   echo 'Not a valid month.'
 fi
+
+case "$arg" in
+
+1 | 3 | 5 | 7 | 8 | 10 | 12)
+  echo 31
+;;
+4 | 6 | 9 | 11)
+  echo 30
+;;
+2)
+  echo 28
+;;
+*)
+  echo 'Not a valid month.'
+
+esac
