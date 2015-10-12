a4a3b0eec946303abc5df63510b3102a1810210d
Ray Gomez <rayvincent.gomez@gmail.com>
2015-10-13 01:13:21 +0800

    implemented password check

--- password-check.sh
+++ password-check.sh
@@ -4,7 +4,47 @@ echo 'Enter password:'
 read password
 
 error=""
+echo "This is the password:${password}"
 
-echo "This is the password:$password, length=${#password}"
+if [ "${#password}" -lt 12 ]
+then
+ error+="Length should be 12 or greater.\n"
+fi
 
-if [ "${#password}" ]
+filtered="${password//[^A-Za-z]}"
+if [ "${#filtered}" -lt 5 ]
+then
+ error+="Must contain at least 5 letters.\n"
+fi
+
+filtered="${password//[^A-Z]}"
+if [ "${#filtered}" -lt 1 ]
+then
+ error+="Must contain at least 1 capital letter.\n"
+fi
+
+filtered="${password//[^a-z]}"
+if [ "${#filtered}" -lt 1 ]
+then
+ error+="Must contain at least 1 small caps letter.\n"
+fi
+
+filtered="${password//[^0-9]}"
+if [ "${#filtered}" -lt 1 ]
+then
+ error+="Must contain at least 1 number.\n"
+fi
+
+filtered="${password//[^@\#\$%\*\+\-\=]}"
+if [ "${#filtered}" -lt 1 ]
+then
+ error+="Must contain at least one of the following @ # $ % * + - =.\n"
+fi
+
+if [ "${error}" ]
+then
+    echo -e "\nPassword is weak."
+    echo -e "${error}"
+else
+    echo -e "\nPassword is strong."
+fi
